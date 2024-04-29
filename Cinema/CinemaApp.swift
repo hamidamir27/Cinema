5731643d71a31fafded3cb41a3f50567c96fe65e
felixlush <92777513+felixlush@users.noreply.github.com>
2024-04-29 10:07:04 +1000

    Initial Commit

---
+++ Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by Felix Lush on 29/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
